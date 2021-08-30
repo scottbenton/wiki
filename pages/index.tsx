329131e0c2dfdec576902e63823902068f1edd7c
Scott Benton <scott@scottbenton.dev>
2021-08-29 23:30:18 -0400

    feat(branding): Added branding to header and favicon (#5)

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,10 @@ const Home: React.FC = (props) => {
     <PageLayout>
       <Head>
         <title>Wiki App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes={"any"} />
+        <link rel="icon" href="/icon.svg" type={"image/svg+xml"} />
+        <link rel="apple-touch-icon" href={"/apple-touch-icon.png"} />
+        <link rel="manifest" href={"/manifest.webmanifest"} />
         <meta name="author" content="Scott Benton" />
       </Head>
       <h1 className="text-red-500">Test Application</h1>
